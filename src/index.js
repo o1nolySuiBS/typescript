40a32a27fff1a70f9fcb657f5aa43fde08958da4
ihor1 <ihor1006@gmail.com>
2024-05-24 18:01:51 +0300

    lesson_1

---
+++ src/index.js
@@ -0,0 +1,17 @@
+var greeting = 'Hello';
+console.log(greeting);
+var sergiy = {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+    address: "adsd",
+    street: "Douglas Extension",
+    suite: "Suite 847",
+    city: 'McKenziehaven',
+    zipcode: 59590 - 4157,
+    geo: {
+        lat: "-68.6102",
+        lng: "-47.0653"
+    },
+};
---
+++ src/index.ts
@@ -0,0 +1,46 @@
+const greeting = 'Hello'
+console.log(greeting)
+// {
+//     "id": 3,
+//     "name": "Clementine Bauch",
+//     "username": "Samantha",
+//     "email": "Nathan@yesenia.net",
+//     "address": {
+//     "street": "Douglas Extension",
+//         "suite": "Suite 847",
+//         "city": "McKenziehaven",
+//         "zipcode": "59590-4157",
+//         "geo": {
+//         "lat": "-68.6102",
+//             "lng": "-47.0653"
+//     }
+// }
+interface user <T> {
+    id: number
+    name: string
+    username: string
+    email: T
+    address: string
+}
+interface location {
+    street: string
+    suite: string
+    city: string
+    zipcode?: number
+    geo: {}
+}
+const sergiy: user <string> & location = {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+    address: "adsd",
+    street: "Douglas Extension",
+    suite: "Suite 847",
+    city: 'McKenziehaven',
+    zipcode: 59590-4157,
+    geo: {
+    lat: "-68.6102",
+    lng: "-47.0653"
+    },
+}
